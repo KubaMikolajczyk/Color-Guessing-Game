7339468b96388aa30253c468cc183e803f8c036d
KubaMikolajczyk <qba600@gmail.com>
2018-08-22 11:15:39 +0200

    add random color generate logic

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,14 +1,7 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square")
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -39,5 +32,31 @@ function changeColors(color){
     // change each colors
     squares[i].style.backgroundColor = color;
   }
+}
 
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random]
+}
+
+function generateRandomColors(num){
+  // make an array
+  var arr = [];
+  // add num random colors to array
+  for(var i = 0; i < num; i++){
+    // get random colors and push into array
+    arr.push(randomColor());
+  }
+  // return that array
+  return arr;
+}
+
+function randomColor(){
+  // pick red 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  // pick green 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  // pick blue 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
