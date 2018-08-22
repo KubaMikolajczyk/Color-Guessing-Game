dba2935e962320240377443858ea8e94334bc2c1
KubaMikolajczyk <qba600@gmail.com>
2018-08-22 12:29:59 +0200

    Add reset button logic

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -4,6 +4,22 @@ var squares = document.querySelectorAll(".square")
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset")
+
+resetButton.addEventListener("click", function(){
+  // generate all new Colors
+  colors = generateRandomColors(6);
+  // pick new random Color
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  // change colors of squares
+  for(var i =0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+  this.textContent = "New Colors";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,8 +33,10 @@ for(var i=0; i<squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     // compare color to picked one
     if(clickedColor === pickedColor){
-      messageDisplay.textContent = "Correct"
+      messageDisplay.textContent = "Correct";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else{
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again"
