17def409ae98b9353b1cef3c68ac881e963ba00c
KubaMikolajczyk <qba600@gmail.com>
2018-08-22 10:41:45 +0200

    set up hint message

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -10,6 +10,7 @@ var colors = [
 var squares = document.querySelectorAll(".square")
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,10 @@ for(var i=0; i<squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     // compare color to picked one
     if(clickedColor === pickedColor){
-      alert("correct")
+      messageDisplay.textContent = "Correct"
     } else{
-      alert("wrong")
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
     }
   });
 }
